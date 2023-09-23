3a4c96a92fe64c4c92cc8aeb78fae578bb2f23cb
sub joel <89309881+Subjoel@users.noreply.github.com>
2023-09-23 14:35:14 +0200

    Update joiner.py

--- joiner.py
+++ joiner.py
@@ -1,10 +1,13 @@
 import requests
+y = 0
 def GetProxies():
     with open('proxies.txt', 'r') as temp_file:
         proxies = [line.rstrip('\n') for line in temp_file]
     return proxies
 
 link = input('Discord Invite Link: ')
+amount = input('Number of users to join: ')
+amount = int(amount)
 if len(link) > 6:
     link = link[19:]
 apilink = "https://discordapp.com/api/v9/invite/" + str(link)
@@ -14,9 +17,17 @@ print (link)
 with open('tokens.txt','r') as handle:
         tokens = handle.readlines()
         for x in tokens:
-            token = x.rstrip()
-            headers={
-                'Authorization': token
-                }
-            requests.post(apilink, headers=headers, proxies={"https://': 'http://" + GetProxies()})
-        print ("All valid tokens have joined!")
+            if y == amount:
+                print("All valid tokens have joined!")
+            else:
+                try:
+                    token = x.rstrip()
+                    headers={
+                        'Authorization': token
+                        }
+                    requests.post(apilink, headers=headers, proxies={"https://': 'http://" + GetProxies()})
+                    y = y + 1
+                    print(f"==>  joined {x} to {link}")
+                except Exception as e:
+                    print(f"==>  Error with {x}, full error is: {e")
+        
